e2790110cd9debdda44f77b84c76de7a055f0e7f
Gakis Van Kef <93915145+Jorg29@users.noreply.github.com>
2022-03-14 00:38:36 +0200

    Delete 2.py

--- 2.py
+++
@@ -1,5 +0,0 @@
-import re
-string="How I need a drink alcoholic of course, after all those lectures involving quantum mechanics"
-print("How I need a drink alcoholic of course, after all those lectures involving quantum mechanics")
-res = len(re.findall(r'\w+', string))
-print("The number of Characters in string are : ", len(string))
