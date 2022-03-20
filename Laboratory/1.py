00a8568635360409e7e1872db1e123f6e85f551f
Gakis Van Kef <93915145+Jorg29@users.noreply.github.com>
2022-03-20 18:58:23 +0200

    Update 1.py

--- Laboratory/1.py
+++ Laboratory/1.py
@@ -1,3 +1,5 @@
+#1)Write a program that calculates the first 10 digits of the result of the following operation: square(2^(101)/pi ^(53) 11^7)
+
 import math
 number=math.sqrt(2**101/(math.pi**53*11**7))
 number_str = str(number)
