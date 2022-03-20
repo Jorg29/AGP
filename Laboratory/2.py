1a57309d3bcf05d89815822bfca6e1b1b0f6fa02
Gakis Van Kef <93915145+Jorg29@users.noreply.github.com>
2022-03-20 18:58:51 +0200

    Update 2.py

--- Laboratory/2.py
+++ Laboratory/2.py
@@ -1,3 +1,5 @@
+#2)Write a program that displays for the string "How I need a drink alcoholic of course, after all those lectures involving quantum mechanics" the number of characters of each word.
+
 import re
 string="How I need a drink alcoholic of course, after all those lectures involving quantum mechanics"
 print("How I need a drink alcoholic of course, after all those lectures involving quantum mechanics")
